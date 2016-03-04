2766a3f1e29276620686af916ed1bb28e0401190
rsaggio <renan.saggio@gmail.com>
2016-03-04 03:47:57 -0300

    capitulo 1

---
+++ EF7Test/Program.cs
@@ -0,0 +1,22 @@
+﻿using EF7Test.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EF7Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EntidadesContext contexto = new EntidadesContext();
+            contexto.Database.EnsureCreated();
+            Usuario usuario = new Usuario() { Nome = "Renan", Senha = "123" };
+            contexto.Usuarios.Add(usuario);
+            contexto.SaveChanges();
+            contexto.Dispose();
+        }
+    }
+}
